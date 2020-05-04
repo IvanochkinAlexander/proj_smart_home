59b9e77daaa35cba231d3b570eef917bd3293bb6
alexanderivanochkin <kasperrrok@gmail.com>
2020-05-04 21:34:50 +0100

    added co2 and humidity scripts

---
+++ src/co2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import RPi.GPIO as GPIO
+PIN_PWM = 21
+
+def getPwm():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(PIN_PWM,GPIO.IN)
+	time.sleep(0.2)
+
+	while GPIO.input(PIN_PWM) == 1:
+		last_high = time.time()
+	while GPIO.input(PIN_PWM) == 0:
+		last_low = time.time()
+	while GPIO.input(PIN_PWM) == 1:
+		last_high = time.time()
+
+	span_high = (last_high - last_low) * 1000
+	#print("span_high : " + str(span_high))
+
+	while GPIO.input(PIN_PWM) == 0:
+		last_low = time.time()
+	while GPIO.input(PIN_PWM) == 1:
+		last_high = time.time()
+	while GPIO.input(PIN_PWM) == 0:
+		last_low = time.time()
+
+	span_low = (last_low - last_high) * 1000
+	#print("span_low : " + str(span_low))
+
+	#print("Cycle : " + str(span_high + span_low))
+
+	CO2 = 5000 * ( span_high - 2 ) / ( span_high + span_low - 4 )
+	AQ = "%.0f" % CO2
+	GPIO.cleanup()
+  return AQ
+
+print (getPwm())
