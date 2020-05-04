a0988ce727d846310cd7335f9452abf4fca6b100
Иваночкин Александр Александрович <ivanochkin.a.a@sberbank.ru>
2020-05-05 00:17:30 +0300

    uncommented useless

--- src/run_home.py
+++ src/run_home.py
@@ -23,11 +23,12 @@ config = load_config(os.path.join(PROJECT_PATH, CONFIG_PATH, 'tokens.yaml'))
 for i in config.keys():
     globals()[i] = get_ip_and_token(i, config)
 
-print(floor_lamp, humidifier, recirculator, chandelier_1, chandelier_2)
+#print(floor_lamp, humidifier, recirculator, chandelier_1, chandelier_2)
 co2 = get_co2()
 print(co2)
 h, t = get_humidity()
 print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(t, h))
+print(t, h)
 # run_command(humidifier, 'on')
 # time.sleep(7)
 # run_command(humidifier, 'off')
